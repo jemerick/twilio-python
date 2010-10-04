40541a300b27800328fb5b08cbf7bdfab7effdf1
jemerick <jemerick@gmail.com>
2010-10-03 21:38:30 -0400

    update rest examples

--- examples/example-rest.py
+++ examples/example-rest.py
@@ -18,14 +18,8 @@ account = twilio.Account(ACCOUNT_SID, ACCOUNT_TOKEN)
 # ===========================================================================
 # 1. Initiate a new outbound call to 415-555-1212
 #    uses a HTTP POST
-d = {
-    'From' : CALLER_ID,
-    'To' : '415-555-1212',
-    'Url' : 'http://demo.twilio.com/welcome',
-}
 try:
-    print account.request('/%s/Accounts/%s/Calls' % \
-                              (API_VERSION, ACCOUNT_SID), 'POST', d)
+    print account.make_call(to_number='415-555-1212', from_number=CALLER_ID, url='http://demo.twilio.com/welcome')
 except Exception, e:
     print e
     print e.read()
@@ -33,10 +27,8 @@ except Exception, e:
 # ===========================================================================
 # 2. Get a list of recent completed calls (i.e. Status = 2)
 #    uses a HTTP GET
-d = { 'Status':2, }
 try:
-    print account.request('/%s/Accounts/%s/Calls' % \
-                              (API_VERSION, ACCOUNT_SID), 'GET', d)
+    print account.get_calls(status='2')
 except Exception, e:
     print e
     print e.read()
@@ -45,8 +37,7 @@ except Exception, e:
 # 3. Get a list of recent notification log entries
 #    uses a HTTP GET
 try:
-    print account.request('/%s/Accounts/%s/Notifications' % \
-                              (API_VERSION, ACCOUNT_SID), 'GET')
+    print account.get_notifications()
 except Exception, e:
     print e
     print e.read()
@@ -54,10 +45,8 @@ except Exception, e:
 # ===========================================================================
 # 4. Get a list of audio recordings for a certain call
 #    uses a HTTP GET
-d = { 'CallSid':'CA0c7001f3f3f5063b7f7d96def0f1ed00', }
 try:
-    print account.request('/%s/Accounts/%s/Recordings' % \
-                              (API_VERSION, ACCOUNT_SID), 'GET', d)
+    print account.get_recordings(call_sid='CA0c7001f3f3f5063b7f7d96def0f1ed00')
 except Exception, e:
     print e
     print e.read()
@@ -66,9 +55,7 @@ except Exception, e:
 # 5. Delete a specific recording
 #    uses a HTTP DELETE, no response is returned when using DELETE
 try:
-    account.request( \
-        '/%s/Accounts/%s/Recordings/RE4e75a0b62a5c52e5cb96dc25fb4101d9' % \
-            (API_VERSION, ACCOUNT_SID), 'DELETE')
+    account.delete_recording(recording_sid='RE4e75a0b62a5c52e5cb96dc25fb4101d9')
 except Exception, e:
     print e
     print e.read()
